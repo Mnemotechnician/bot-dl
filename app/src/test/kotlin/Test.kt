21a53432aa59c32770eb7a90c606b70b6afca606
MHeMoTexHuK <daytimer253@gmail.com>
2022-03-16 15:45:27 +0300

    ooooops, i forgot to add the test itself.

---
+++ app/src/test/kotlin/Test.kt
@@ -0,0 +1,11 @@
+package com.github.mnemotechnician.botdl.test
+
+import kotlin.test.*
+import com.github.mnemotechnician.botdl.dataset.*
+
+class AppTest {
+	@Test
+	fun `dataset created succefully`() {
+		PhraseDataset.split(0.5)
+	}
+}
